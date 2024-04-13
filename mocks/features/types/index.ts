bbd8ed77a029dee44c618785ab33c972d4f9ea29
kobanago <kobanago3800@gmail.com>
2024-04-13 12:31:35 +0900

    mocks：update：goods data

--- mocks/features/types/index.ts
+++ mocks/features/types/index.ts
@@ -1,7 +1,11 @@
 export type GoodsType = {
+  id: number;
   name: string;
   price: number;
   label: string;
   sex: string;
   src: string;
+  size: string[];
+  stock: number[];
+  created_at: number;
 };
